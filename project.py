2015c4054c0da4e75fe085ba87a8029e8b7f3915
suryapratap83 <suryapratap8315@gmail.com>
2026-06-30 22:18:53 +0530

    Add option to generate new password with new length and improve input validation

--- project.py
+++ project.py
@@ -1,45 +1,57 @@
 import random
 import string
 
-n = int(input("Enter length: "))
-while n <= 0:
-    n = int(input("Enter a valid length (greater than 0): "))
-
 chars = ""
 
-while chars == "":
-    digits = input("Include digits? (y/n): ")
-    uppercase = input("Include uppercase? (y/n): ")
-    lowercase = input("Include lowercase? (y/n): ")
-    punctuation = input("Include symbols? (y/n): ")
-
-    if digits.lower() == "y":
-        chars += string.digits
-    if uppercase.lower() == "y":
-        chars += string.ascii_uppercase
-    if lowercase.lower() == "y":
-        chars += string.ascii_lowercase
-    if punctuation.lower() == "y":
-        chars += string.punctuation
+while True:
 
     if chars == "":
-        print("Please select at least one character type!")
-while True:
-    password= ""
+        while chars == "":
+            digits = input("Include digits? (y/n): ")
+            uppercase = input("Include uppercase? (y/n): ")
+            lowercase = input("Include lowercase? (y/n): ")
+            punctuation = input("Include symbols? (y/n): ")
+
+            if digits.lower() == "y":
+                chars += string.digits
+            if uppercase.lower() == "y":
+                chars += string.ascii_uppercase
+            if lowercase.lower() == "y":
+                chars += string.ascii_lowercase
+            if punctuation.lower() == "y":
+                chars += string.punctuation
+
+            if chars == "":
+                print("Please select at least one character type!")
+
+    while True:
+        try:
+            n = int(input("Enter password length: "))
+            if n > 0:
+                break
+            print("Length must be greater than 0.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+    password = ""
+
     for i in range(n):
         password += random.choice(chars)
-    print("Your password:", password)
-    
+
+    print("\nGenerated Password:", password)
+
+    print("\n1. Generate another password")
+
+    print("2. Exit")
+
     while True:
-        
-     choice = input("Another password (y/n): ").lower()
+        choice = input("\nGenerate another password? (1/2): ").lower()
 
-     if choice == "y":
-          break
+        if choice == "1":
+            break     
+        elif choice == "2":
+            print("Goodbye!")
+            exit()
+        else:
+            print("choose proper menu")
 
-     elif choice == "n":
-        print("Goodbye")
-        exit()
-
-     else:
-        print("Enter valid choice")
